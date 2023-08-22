40b682a08bcb971794fdf0a6341f6e802d878fb0
Ciro Bispo <cirobispo@zoho.com>
2023-08-22 19:18:31 -0300

    largest-series-product complete

--- go/largest-series-product/largest_series_product.go
+++ go/largest-series-product/largest_series_product.go
@@ -1,5 +1,35 @@
 package lsproduct
 
+import (
+	"errors"
+	"strconv"
+)
+
 func LargestSeriesProduct(digits string, span int) (int64, error) {
-	panic("Please implement the LargestSeriesProduct function")
+	digCount := len(digits)
+	serieCount := digCount - span + 1
+	if digCount >= span && span > 0 {
+		var bestProduct int64 = 0
+		series := make(map[string]int64, serieCount)
+		for i := 0; i < serieCount; i++ {
+			serie := digits[i : i+span]
+			for _, c := range serie {
+				if v, err := strconv.Atoi(string(c)); err == nil {
+					if _, f := series[serie]; !f {
+						series[serie] = 1
+					}
+					series[serie] = series[serie] * int64(v)
+				} else {
+					return 0, errors.New("conversion error")
+				}
+			}
+
+			if series[serie] > bestProduct {
+				bestProduct = series[serie]
+			}
+		}
+		return bestProduct, nil
+	}
+
+	return 0, errors.New("impossible make series")
 }
