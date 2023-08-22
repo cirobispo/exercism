64268299a69d76544480640ec7d7b52dde06d5e0
Ciro Bispo <cirobispo@zoho.com>
2023-08-22 14:23:08 -0300

    Anagram complete

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,5 +1,46 @@
 package anagram
 
+import (
+	"strings"
+)
+
+func buildCharMap(word *string) map[rune]int {
+	var result map[rune]int = make(map[rune]int)
+	for _, c := range *word {
+		result[c] += 1
+	}
+
+	return result
+}
+
+func compareCharMap(l, r map[rune]int) bool {
+	if len(l) == len(r) {
+		result := true
+		for k, v := range l {
+			if search, found := r[k]; !found || v != search {
+				result = false
+				break
+			}
+		}
+		return result
+	}
+	return false
+}
+
 func Detect(subject string, candidates []string) []string {
-	panic("Please implement the Detect function")
+	text := strings.ToLower(subject)
+	chars_sub := buildCharMap(&text)
+
+	var result = make([]string, 0)
+	for _, w := range candidates {
+		word := strings.ToLower(w)
+		if word != text {
+			chars_chk := buildCharMap(&word)
+			if compareCharMap(chars_sub, chars_chk) {
+				result = append(result, w)
+			}
+		}
+	}
+
+	return result
 }
