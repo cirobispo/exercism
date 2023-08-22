f01d847a355e0c3170a4597928d424fc3c25493c
Ciro Bispo <cirobispo@zoho.com>
2023-08-22 16:56:25 -0300

    word count complete

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,7 +1,22 @@
 package wordcount
 
+import (
+	"regexp"
+	"strings"
+)
+
 type Frequency map[string]int
 
 func WordCount(phrase string) Frequency {
-	panic("Please implement the WordCount function")
+	if re, err := regexp.Compile(`(\w+'t|\w+'s|\w+'re)|\w+`); err == nil {
+		words := re.FindAllString(strings.ToLower(phrase), -1)
+		var freq Frequency = make(Frequency)
+		for _, word := range words {
+			freq[word] += 1
+		}
+
+		return freq
+	}
+
+	return Frequency{}
 }
