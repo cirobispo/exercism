8fd3d295cb1cf9c7cc96905e94f3634341c0f74d
Ciro Bispo <cirobispo@zoho.com>
2023-09-06 16:01:54 -0300

    armstrong complete

--- go/armstrong-numbers/armstrong_numbers.go
+++ go/armstrong-numbers/armstrong_numbers.go
@@ -1,5 +1,27 @@
 package armstrong
 
+import (
+	"math"
+)
+
 func IsNumber(n int) bool {
-	panic("Please implement the IsNumber function")
+	result := true
+	if n > 0 {
+		exp := int(math.Ceil(math.Log10(float64(n))))
+		if n%10 == 0 {
+			exp++
+		}
+
+		var sum float64
+		var remvalue int = n
+		for i := 1; i <= exp; i++ {
+			divisor := math.Pow(10, float64(exp-i))
+			base := int(float64(remvalue) / divisor)
+			sum += math.Pow(float64(base), float64(exp))
+			remvalue -= int(divisor * float64(base))
+		}
+
+		result = (int(sum) == n)
+	}
+	return result
 }
