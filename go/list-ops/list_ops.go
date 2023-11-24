6e79783c2d57cd74bf498f1ca5808e252e283642
Ciro Bispo <cirobispo@zoho.com>
2023-11-24 18:09:06 -0300

    list-ops complete

--- go/list-ops/list_ops.go
+++ go/list-ops/list_ops.go
@@ -4,33 +4,74 @@ package listops
 type IntList []int
 
 func (s IntList) Foldl(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldl function")
+	result := initial
+	for i := 0; i < s.Length(); i++ {
+		result = fn(result, s[i])
+	}
+
+	return result
 }
 
 func (s IntList) Foldr(fn func(int, int) int, initial int) int {
-	panic("Please implement the Foldr function")
+	result := initial
+	for i := s.Length() - 1; i > -1; i-- {
+		result = fn(s[i], result)
+	}
+
+	return result
 }
 
 func (s IntList) Filter(fn func(int) bool) IntList {
-	panic("Please implement the Filter function")
+	result := make(IntList, 0)
+	for i := range s {
+		if fn(s[i]) {
+			result = append(result, s[i])
+		}
+	}
+
+	return result
 }
 
 func (s IntList) Length() int {
-	panic("Please implement the Length function")
+	return len(s)
 }
 
 func (s IntList) Map(fn func(int) int) IntList {
-	panic("Please implement the Map function")
+	result := make(IntList, 0)
+	for i := range s {
+		result = append(result, fn(s[i]))
+	}
+
+	return result
 }
 
 func (s IntList) Reverse() IntList {
-	panic("Please implement the Reverse function")
+	result := make(IntList, 0, s.Length())
+	for i := s.Length() - 1; i >= 0; i-- {
+		result = append(result, s[i])
+	}
+
+	return result
 }
 
 func (s IntList) Append(lst IntList) IntList {
-	panic("Please implement the Append function")
+	result := make(IntList, 0, s.Length()+lst.Length())
+	result = append(result, s...)
+	result = append(result, lst...)
+	return result
 }
 
 func (s IntList) Concat(lists []IntList) IntList {
-	panic("Please implement the Concat function")
+	size := s.Length()
+	for i := range lists {
+		size += lists[i].Length()
+	}
+
+	result := make(IntList, 0, size)
+	result = append(result, s...)
+	for i := range lists {
+		result = append(result, lists[i]...)
+	}
+
+	return result
 }
