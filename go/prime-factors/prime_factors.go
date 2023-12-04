7ba6a30a20fb3da401d9e719da57856428f386c0
Ciro Bispo <oabispo@zoho.com>
2023-12-04 16:16:28 -0300

    prime-factors complete

--- go/prime-factors/prime_factors.go
+++ go/prime-factors/prime_factors.go
@@ -1,6 +1,14 @@
 package prime
 
+import (
+	"sort"
+	"sync"
+)
+
 type PrimeNumbers []int64
+type PAV struct {
+	position, acumulator, foundValue int64
+}
 
 func (pn PrimeNumbers) IsPrime(value int64) (int64, bool) {
 	result, OK:=value, true
@@ -18,39 +26,107 @@ func (pn *PrimeNumbers) Add(value int64) {
 	*pn=append(*pn, value)
 }
 
-func(pn PrimeNumbers) SearchNewOne() (int64, bool) {
-	size:=len(pn)
-	lastOne:=pn[size-1]
-	divisors:=[]int64{1}
-	divisors=append(divisors, pn...)
-	for i:=0; i<size; i++ {
-		nums2Sum:=1
-		for nums2Sum+i < size {
-			var soma int64=0
-			for j:=i;j<i+nums2Sum; j++ {
-				soma+=divisors[j]
-			}
-			nums2Sum++
-			if value, is:=pn.IsPrime(lastOne + soma); is {
-				return value, true
+func(pn PrimeNumbers) ResearchNewPrime(pavs []PAV) ([]int64, bool) {
+	search:=func(wg *sync.WaitGroup, pav *PAV, found chan bool) {
+		defer wg.Done()
+
+		for i:=pav.position; i< pav.position+500; i+=pav.acumulator {
+			select {
+			case <-found:
+				goto quit
+			default:
+				if value, f:=pn.IsPrime(i); f {
+					pav.foundValue=value
+					goto quit
+				}
 			}
 		}
+quit:
+		found<- true
+		//Avise ao proximo!
+		<-found
 	}
-	return 0, false
+
+	result:=make(map[int64]int)
+	size:=len(pavs)
+	found:=make(chan bool, size)
+	var wg sync.WaitGroup
+	for i:=0; i< size; i++ {
+		pav:=&pavs[i]
+		wg.Add(1)
+		go search(&wg, pav, found)		
+	}
+	wg.Wait()
+
+	for i:=range pavs {
+		if pavs[i].foundValue > 0 {
+			result[pavs[i].foundValue]++
+		}
+	}
+
+	keys := make([]int64, 0, len(result))
+	for k := range result {
+		keys = append(keys, k)
+	}
+	
+	return keys, len(keys) > 0
 }
 
 func subDivisors(n int64, pn PrimeNumbers) []int64 {
 	result:=[]int64{}
-again:
-	if value, isPrime:=pn.IsPrime(n); !isPrime {
-		if value > 1 {
-			result = append(result, value)
-			result = append(result, subDivisors(n/value, pn)...)
+
+	getPAVs:=func(n int64, pavCount int, pavs []PAV) []PAV {
+		size:=len(pavs)
+		if size == 0 { 
+			biggestPrime:=pn[len(pn)-1]
+			pav:=PAV{position: biggestPrime}
+			pavs=[]PAV{pav, pav, pav}
+			size=len(pavs)
+			for i:=0 ; i<size; i++ {
+				pavs[i].acumulator=pn[i]
+			}
 		}
-	} else {
-		if value, is:=pn.SearchNewOne(); is {
-			pn.Add(value)
-			goto again
+
+		result:=make([]PAV, 0, size)
+		for i:=0; i<size; i++ {
+			newPosition:=pavs[i].position
+			if pavs[i].foundValue > 0 {
+				newPosition=pavs[i].foundValue
+				if newPosition > n {
+					newPosition=pn[i + size]
+					pavs[i].acumulator=pn[i + size]
+				}
+			}
+			pav:=PAV{position: newPosition, acumulator: pavs[i].acumulator, foundValue: 0}
+			result = append(result, pav)
+		}
+
+		return result
+	}
+
+
+	i:=n
+
+	pavs:=getPAVs(i, 6, []PAV{})
+
+	for i > 1 {
+		if value, isPrime:=pn.IsPrime(i); !isPrime {
+			result = append(result, value)
+			i=i/value
+		} else {
+			for {
+				if value, is:=pn.ResearchNewPrime(pavs); is {
+					for j:=range value {
+						pn.Add(value[j])
+					}
+					sort.Slice(pn, func(i, j int) bool {
+						return pn[i] < pn[j]
+					})
+
+					pavs=getPAVs(i, 6, pavs)
+					break
+				}
+			}
 		}
 	}
 
