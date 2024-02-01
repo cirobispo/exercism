5253bf040a09e86623826e44079a9245e3387979
Ciro Bispo <cirobispo@zoho.com>
2024-02-01 15:24:52 -0300

    word-search same code, 2 lines together

--- go/word-search/word_search.go
+++ go/word-search/word_search.go
@@ -52,8 +52,7 @@ func (p puzzleMapping) checkWordIn(word string, rowPos, colPos, vWay, hWay int) 
 	}
 
 	result := make([][]int, 0)
-	if found {
-		size := len(word)
+	if size := len(word); found { // just to let code 1 line smaller
 		result = append(result, []int{colPos, rowPos})
 		result = append(result, []int{colPos + (size-1)*hWay, rowPos + (size-1)*vWay})
 	}
