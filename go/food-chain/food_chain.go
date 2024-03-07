311628e492eb166f0092c6856e1e69b2bc0071bb
Ciro Bispo <cirobispo@zoho.com>
2024-03-07 13:05:49 -0300

    food-chain

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -1,12 +1,5 @@
 package foodchain
 
-<<<<<<< HEAD
-//
-// HOW I HATE THIS STUPID SONGS. BUNCH OF CRAP TEXT TO PUT TOGETHER
-//
-
-=======
->>>>>>> 3e7b859 (food-chain complete)
 import (
 	"fmt"
 	"strings"
@@ -32,13 +25,10 @@ func buildAnimalList() []animal {
 	result = append(result, animal{name: "goat", exclamation: "Just opened her throat and swallowed a goat!"})
 	result = append(result, animal{name: "cow", exclamation: "I don't know how she swallowed a cow!"})
 	result = append(result, animal{name: "horse", exclamation: "She's dead, of course!"})
-
 	return result
 }
-
 func Verse(v int) string {
 	animals := buildAnimalList()
-
 	var sb strings.Builder
 	if v > 0 && v < len(animals) {
 		if v > 1 && v < 8 {
@@ -60,25 +50,20 @@ func Verse(v int) string {
 	result := sb.String()
 	return result
 }
-
 func Verses(start, end int) string {
 	var sb strings.Builder
 	for i := start; i <= end; i++ {
 		sb.WriteString("\n\n")
 		sb.WriteString(Verse(i))
 	}
-
 	return sb.String()[2:]
 }
-
 func Song() string {
-	return Verses(1, 9)
+	return Verses(1, 8)
 }
-
 func sheSwallowed(swallow animal) string {
 	return fmt.Sprintf("I know an old lady who swallowed a %s.\n", swallow.name)
 }
-
 func sheComplain(swallow animal, extra string, brake bool) string {
 	result := swallow.exclamation
 	if strings.Contains(swallow.exclamation, "%s") {
@@ -90,16 +75,13 @@ func sheComplain(swallow animal, extra string, brake bool) string {
 	}
 	return result
 }
-
 func sheSwallowedToCatch(swallow, catch animal) string {
 	result := fmt.Sprintf("She swallowed the %s to catch the %s.\n", swallow.name, catch.name)
 	if swallow.name == "bird" {
 		result = fmt.Sprintf("She swallowed the %s to catch the %s %s\n", swallow.name, catch.name, sheComplain(catch, "that", false))
 	}
-
 	return result
 }
-
 func sheWillDie(swallow animal) string {
 	return fmt.Sprintf("I don't know why she swallowed the %s. Perhaps she'll die.", swallow.name)
 }
