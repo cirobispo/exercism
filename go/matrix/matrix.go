d312d1569a88131fb83e98deeb11bb3e6a4c49a7
Ciro Bispo <cirobispo@zoho.com>
2024-03-07 12:34:07 -0300

    matrix complete

--- go/matrix/matrix.go
+++ go/matrix/matrix.go
@@ -1,85 +1,89 @@
 package matrix
 
 import (
-	"errors"
+	"fmt"
 	"strconv"
 	"strings"
 )
 
 // Define the Matrix type here.
-type Matrix struct {
-	columns, lines int
-	values         []int
-}
+type Matrix [][]int
 
-func New(s string) (*Matrix, error) {
+func New(s string) (Matrix, error) {
 	lines := strings.Split(s, "\n")
-
-	buildResult := func(lines []string) (*Matrix, error) {
-		var result *Matrix
-		var err = errors.New("error building matrix")
-		//defining its size
-		if ls := len(lines); ls > 0 {
-			for i := range lines {
-				line := lines[i]
-				columns := strings.Split(line, " ")
-				if cs := len(columns); cs > 0 {
-					result = &Matrix{lines: ls, columns: cs}
-					err = nil
-				}
-			}
+	result := make(Matrix, 0, len(lines))
+	cellsSize := -1
+	for i := range lines {
+		line := strings.TrimSpace(lines[i])
+		if len(line) == 0 {
+			return [][]int{}, fmt.Errorf("some line has no cells")
 		}
 
-		return result, err
-	}
-
-	if result, err := buildResult(lines); err == nil {
-		for i := range lines {
-			line := lines[i]
-			columns := strings.Split(line, " ")
-			for j := range columns {
-				value, _ := strconv.ParseInt(columns[j], 10, 64)
-				result.values = append(result.values, int(value))
-			}
+		cells := strings.Split(line, " ")
+		row := make([]int, 0, len(cells))
+		if size := len(cells); cellsSize != -1 && size != cellsSize {
+			return [][]int{}, fmt.Errorf("lines have different cell count")
+		} else {
+			cellsSize = size
 		}
-
-		return result, nil
-	} else {
-		return &Matrix{}, err
+		for j := range cells {
+			cell := cells[j]
+			value, err := strconv.Atoi(cell)
+			if err != nil {
+				return [][]int{}, fmt.Errorf("value %v is not a proper int", cell)
+			}
+			row = append(row, value)
+		}
+		result = append(result, row)
 	}
+
+	return result, nil
 }
 
 // Cols and Rows must return the results without affecting the matrix.
-func (m *Matrix) Cols() [][]int {
-	result := make([][]int, 0, m.lines*m.columns)
+func (m Matrix) Cols() [][]int {
+	lineCount := len(m)
+	if lineCount < 1 {
+		return [][]int{}
+	}
 
-	for i := 0; i < m.columns; i++ {
-		for j := 0; j < m.lines; j++ {
-			result[i][j] = m.values[(j*m.columns)+i]
+	cellCount := len(m[0])
+	result := make([][]int, 0, cellCount)
+	for c := 0; c < cellCount; c++ {
+		result = append(result, []int{})
+		for l := 0; l < lineCount; l++ {
+			result[c] = append(result[c], m[l][c])
 		}
 	}
 
 	return result
 }
 
-func (m *Matrix) Rows() [][]int {
-	result := make([][]int, 0, m.lines*m.columns)
+func (m Matrix) Rows() [][]int {
+	lineCount := len(m)
+	if lineCount < 1 {
+		return [][]int{}
+	}
 
-	for j := 0; j < m.lines; j++ {
-		for i := 0; i < m.columns; i++ {
-			result[j][i] = m.values[(j*m.columns)+i]
-		}
+	result := make([][]int, 0, lineCount)
+	for i := 0; i < lineCount; i++ {
+		result = append(result, []int{})
+		result[i] = append(result[i], m[i]...)
 	}
 
 	return result
 }
 
-func (m *Matrix) Set(row, col, val int) bool {
-	onBoard := (row >= 0 && row < m.lines) && (col >= 0 && col < m.columns)
-	if onBoard {
-		m.values[(row*m.columns)+col] = val
-		return true
+func (m Matrix) Set(row, col, val int) bool {
+	if row < 0 || row > len(m)-1 {
+		return false
 	}
 
-	return false
+	line := m[row]
+	if col < 0 || col > len(line)-1 {
+		return false
+	}
+
+	m[row][col] = val
+	return true
 }
