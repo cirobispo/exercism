8b4b4c0b3cb38d93ba7cf103286958a458d9a7f5
Ciro Bispo <oabispo@zoho.com>
2024-11-21 17:50:36 -0300

    food-chain complete

--- go/food-chain/food_chain.go
+++ go/food-chain/food_chain.go
@@ -27,6 +27,7 @@ func buildAnimalList() []animal {
 	result = append(result, animal{name: "horse", exclamation: "She's dead, of course!"})
 	return result
 }
+
 func Verse(v int) string {
 	animals := buildAnimalList()
 	var sb strings.Builder
@@ -50,6 +51,7 @@ func Verse(v int) string {
 	result := sb.String()
 	return result
 }
+
 func Verses(start, end int) string {
 	var sb strings.Builder
 	for i := start; i <= end; i++ {
@@ -58,12 +60,15 @@ func Verses(start, end int) string {
 	}
 	return sb.String()[2:]
 }
+
 func Song() string {
 	return Verses(1, 8)
 }
+
 func sheSwallowed(swallow animal) string {
 	return fmt.Sprintf("I know an old lady who swallowed a %s.\n", swallow.name)
 }
+
 func sheComplain(swallow animal, extra string, brake bool) string {
 	result := swallow.exclamation
 	if strings.Contains(swallow.exclamation, "%s") {
@@ -75,6 +80,7 @@ func sheComplain(swallow animal, extra string, brake bool) string {
 	}
 	return result
 }
+
 func sheSwallowedToCatch(swallow, catch animal) string {
 	result := fmt.Sprintf("She swallowed the %s to catch the %s.\n", swallow.name, catch.name)
 	if swallow.name == "bird" {
@@ -82,6 +88,7 @@ func sheSwallowedToCatch(swallow, catch animal) string {
 	}
 	return result
 }
+
 func sheWillDie(swallow animal) string {
 	return fmt.Sprintf("I don't know why she swallowed the %s. Perhaps she'll die.", swallow.name)
 }
